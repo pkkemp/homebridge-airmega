44d2763f4e7b2000687be09dc07a80780ede409e
Alex Rosenberg <alexr@leftfield.org>
2022-12-06 12:57:16 -0800

    More eslint complaints

--- src/lib/Config.ts
+++ src/lib/Config.ts
@@ -1,48 +1,46 @@
-export namespace Config {
-  export const BASE_URI = 'https://iocareapp.coway.com/bizmob.iocare';
-  //export const USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1';
-  export const USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1 app';
-  export const ACCEPT = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
-  export const ACCEPT2 = 'application/json, text/plain, */*';
-  export const ACCEPT_LANGUAGE = 'en-US,en;q=0.9';
+/* eslint-disable max-len */
+export const Config = {
+  BASE_URI : 'https://iocareapp.coway.com/bizmob.iocare',
+  USER_AGENT : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1 app',
+  ACCEPT : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  ACCEPT2 : 'application/json, text/plain, */*',
+  ACCEPT_LANGUAGE : 'en-US,en;q=0.9',
 
-  export namespace ContentType {
-    export const FORM = 'application/x-www-form-urlencoded';
-    export const JSON = 'application/json';
-  }
+  ContentType : {
+    FORM : 'application/x-www-form-urlencoded',
+    JSON : 'application/json',
+  },
 
-  export namespace Auth {
-    export const OAUTH_URL = 'https://idp.coway.com/oauth2/v1/authorize';
-    export const OPENID_URL = 'https://id.coway.com/auth/realms/cw-account/protocol/openid-connect/auth';
-    export const SIGNIN_URL = 'https://idp.coway.com/user/signin/';
-    export const REDIRECT_URL = 'https://iocareapp.coway.com/bizmob.iocare/redirect/redirect.html';
-    export const REDIRECT_URL2 = 'https://iocareapp.coway.com/bizmob.iocare/redirect/redirect_bridge.html';
-    export const CLIENT_ID = 'UmVuZXdhbCBBcHA';
-    export const OPENID_CLIENT_ID = 'cwid-prd-iocare-20220930';
-    export const SERVICE_CODE = 'com.coway.IOCareKor';
-    export const COWAY_ACCESS_TOKEN = 'coway_access_token';
-    export const COWAY_REFRESH_TOKEN = 'coway_refresh_token';
-  }
+  Auth : {
+    OPENID_URL : 'https://id.coway.com/auth/realms/cw-account/protocol/openid-connect/auth',
+    SIGNIN_URL : 'https://idp.coway.com/user/signin/',
+    REDIRECT_URL : 'https://iocareapp.coway.com/bizmob.iocare/redirect/redirect.html',
+    REDIRECT_URL2 : 'https://iocareapp.coway.com/bizmob.iocare/redirect/redirect_bridge.html',
+    CLIENT_ID : 'UmVuZXdhbCBBcHA',
+    OPENID_CLIENT_ID : 'cwid-prd-iocare-20220930',
+    SERVICE_CODE : 'com.coway.IOCareKor',
+    COWAY_ACCESS_TOKEN : 'coway_access_token',
+    COWAY_REFRESH_TOKEN : 'coway_refresh_token',
+  },
 
-  export namespace Endpoints {
-    export const DEVICE_LIST = 'CWIG0304';
-    export const DEVICE_REFRESH = 'CWIG0602';
-    export const TOKEN_REFRESH = 'CWIL0100';
-    export const STATUS = 'CWIA0120';
-    export const CONTROL = 'CWIG0603';
-    export const FILTERS = 'CWIA0800';
-  }
+  Endpoints : {
+    DEVICE_LIST : 'CWIG0304',
+    DEVICE_REFRESH : 'CWIG0602',
+    TOKEN_REFRESH : 'CWIL0100',
+    STATUS : 'CWIA0120',
+    CONTROL : 'CWIG0603',
+    FILTERS : 'CWIA0800',
+  },
 
-  export namespace Codes {
-    export const POWER = '0001';
-    export const MODE = '0002';
-    export const FAN = '0003';
-    export const LIGHT = '0007';
-  }
+  Codes : {
+    POWER : '0001',
+    MODE : '0002',
+    FAN : '0003',
+    LIGHT : '0007',
+  },
 
-  export namespace Filters {
-    export const PRE_FILTER = '3121332';
-    // export const MAIN_FILTER = '3111735';
-    export const MAIN_FILTER = '3104756';
-  }
-}
+  Filters : {
+    PRE_FILTER : '3121332',
+    MAIN_FILTER : '3104756', // previously 3111735
+  },
+} as const;
